2cc4732add53a4e4a297104b028b2a7b67fe0245
Jahongir Ortiqov <108238210+foydalanuv@users.noreply.github.com>
2022-10-26 12:51:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,225 @@
+"use strict"
+             //   M-1
+            //   let sm = +prompt("sonni kiriting")   
+            //     sm/=100;
+            //     console.log(sm +" metr");
+
+            //     M-2
+     
+            //   let sm = +prompt("sonni kiriting")   
+            //     sm/=1000;
+            //    console.log(sm +" tonna");
+
+            //     M-3
+
+            //   let a = +prompt("sonni kiriting"),
+            //      b = +prompt("sonni kiriting")
+                
+            //     console.log(Math.floor(a/b));
+        
+            //     M-4
+
+            // let a = +prompt("sonni kiriting")
+            //  console.log("o'nlar xonasi "+Math.floor(a/10) , "birliklar xonasi " + Math.floor(a%10));
+
+            //     M-5
+
+            // let a = +prompt("sonni kiriting")
+            //     console.log(Math.floor(a/10) +  Math.floor(a%10));
+
+            //     M-6
+
+            // let a = +prompt("sonni kiriting")
+            //     console.log(Math.floor(a/10) *  Math.floor(a%10));
+
+            //     M-7
+
+            //    let a = +prompt("sonni kiriting")
+            //       console.log(Math.floor(a%1000));
+
+            //     M-8
+
+            // let a = +prompt("sonni kiriting")
+            //  console.log("kun boshidan " + Math.floor(a/60) + " soatu"  , Math.floor(a%60) + " minut o'tdi" );
+
+                    //  Mantiqiy Amallar
+                
+                    // M-1
+                
+            //  let a = +prompt("sonni kiriting")
+            //     console.log((a*10)%10==0);
+
+                    // M-2
+
+            //  let a = +prompt("sonni kiriting")   
+            //     console.log(a%2!=0)
+
+                 //    M-3
+
+            //   let a = +prompt("sonni kiriting") 
+            //        if(a*10%10==0 && a%2==0){
+            //             console.log("true");
+            //        }else {
+            //             console.log("folse");
+            //        }
+            //         M-4
+
+             // let a = +prompt("sonni kiriting"),
+            //   b = +prompt("sonni kiriting")
+            //   console.log(a*10%10==0 && b*10%10==0);
+
+            //         M-5
+
+                // let a = +prompt("sonni kiriting"),
+                //     b = +prompt("sonni kiriting"),
+                //     c = +prompt("sonni kiriting")
+                //   if(a>b && b>c){
+                //     console.log("true");
+                //   }else{
+                //     console.log("folse");
+                //   }
+
+                 //       M-6  
+
+                    // let a = +prompt("sonni kiriting"),
+                    //     b = +prompt("sonni kiriting"),
+                    //     c = +prompt("sonni kiriting")
+                    //     if(a>b && b>c){
+                    //         console.log("true");
+                    //     }else{
+                    //         console.log("folse");
+                    //     }
+
+                    //     M-7
+                    
+                    
+                        // let a = +prompt("sonni kiriting"),
+                        //     b = +prompt("sonni kiriting")
+
+                        //     if(a%2!=0 && b%2!=0){
+                        //         console.log("toq");
+
+                        //     }else{
+                        //         console.log("juft");
+                        //     }
+
+                        //      M-8
+     
+                        //  let a = +prompt("sonni kiriting"),
+                        //      b = +prompt("sonni kiriting"),
+                        //      c = +prompt("sonni kiriting"),
+                        //      d = +prompt("sonni kiriting")
+                        
+                        // if(a>0 &&  b>0 && c>0 && d>0){
+                        //     console.log("musbat");
+                        // }else{
+                        //     console.log("manfiy");
+                        // }
+    
+                        //        M-9   
+                                    
+                        // let a = +prompt("sonni kiriting"),
+                        //     b = +prompt("sonni kiriting"),
+                        //     c = +prompt("sonni kiriting")
+
+                        //   if(a>0 && b>0 || c>0 && b>0 || a>0 && c>0) {
+                        //     console.log("musbat");
+                        //   } else{
+                        //     console.log("manfiy");
+                        //   }
+
+                        //         M-10
+                            // let a = +prompt("sonni kiriting")
+                          
+                            //  if(a==1){
+                            //     console.log("dushanba");
+                            //  }else if(a==2){
+                            //     console.log("seshanba");
+                            //  }else if(a==3){
+                            //     console.log("chorshanba");
+                            //  }else if(a==4){
+                            //     console.log("payshanba");
+                            //  }else if(a==5){
+                            //     console.log("juma");
+                            //  }else if(a==6){
+                            //     console.log("shanba");
+                            //  } else if(a==7){
+                            //     console.log("yakshanba");
+                            //  } else{
+                            //     console.log("bunday hafta kuni mavjud emas");
+                            //  }
+
+                            //    M-11
+
+                            //   let a = +prompt("sonni kiriting")
+                            //   if(a==94 || a==93){
+                            //     console.log("ucell");
+                            //   }else if(a==90 || a==91){
+                            //     console.log("beline");
+                            //   }else if(a==99){
+                            //     console.log("uzMobile");
+                            //   }else{
+                            //     console.log("bunday kodli kampaniya mavjud emas");
+                            //   }
+
+                            //    M-12
+                             
+                            // let a = +prompt("sonni kiriting")
+                            //     if(a>0){
+                            //         a++
+                            //         console.log(a);
+                            //     }else{
+                            //         a--
+                            //         console.log(a);
+                            //     }
+
+                            //     M-13
+                                //  let a = +prompt("sonni kiriting")
+                                // if(a>0){
+                                //     a+=3
+                                //     console.log(a);
+                                // }else{
+                                //     a-=2
+                                //     console.log(a);
+                                // }
+
+                              //  M-14
+                              
+                            //  let a = +prompt("sonni kiriting"),
+                            //       b = +prompt("sonni kiriting")
+
+                            //      if(a>b){
+                            //           console.log(a);
+                            //         }else{
+                            //              console.log(b);
+                            //         }
+
+
+                            //     M-15
+
+                            
+                            //  let a = +prompt("sonni kiriting"),
+                            //      b = +prompt("sonni kiriting"),
+                            //      c = +prompt("sonni kiriting")
+
+                            //   if(a>b && a>c){
+                            //   console.log(a);
+                            //  } else if(b>a && b>c){
+                            //    console.log(b);
+                            //  }else{
+                            //  console.log(c);
+                            //     }
+
+                            //     M-16
+
+                            //   let a = +prompt("sonni kiriting"),
+                            //       b = +prompt("sonni kiriting"),
+                            //       c = +prompt("sonni kiriting")
+
+                            //   if(a<b && a<c){
+                            //   console.log(a);
+                            //  } else if(b<a && b<c){
+                            //    console.log(b);
+                            //  }else{
+                            //  console.log(c);
+                            //     }
